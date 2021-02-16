2080addee002836baabfea73730986e7f5ff39f5
Rochelle Dai <rmdai@ucdavis.edu>
2021-02-16 01:59:48 -0800

    moved imgs folder

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -18,7 +18,7 @@
         //if the mouse is hovering over casette1,  
         if (e.target == cass1) {
             //change the img and text to casette1 
-            spotimg.src="../images/cass/cass1.png";
+            spotimg.src="cass/cass1.png";
             title.innerHTML=`"Eye Massages"`;
             info.innerHTML=`This casette teaches you four simple massages that promote eye health and reduce stress. Great for the health-conscious individual.`
             //reveal the spotlight div after making the changes
@@ -26,19 +26,19 @@
         }
         else if (e.target == cass2) {
             //changing the img and text to the correct casette
-            spotimg.src="../images/cass/cass2.png";
+            spotimg.src="cass/cass2.png";
             title.innerHTML=`"Protect Your Eyes"`;
             info.innerHTML=`A mysterious voice counts methodically and instructs listeners to practice massaging their face in Chinese on top of ominous instrumentals.`
             spotlight.className="show";
         }
         else if (e.target == cass3) {
-            spotimg.src="../images/cass/cass3.png";
+            spotimg.src="cass/cass3.png";
             title.innerHTML=`"English Test"`;
             info.innerHTML=`Two English speakers converse in an extremely scripted manner about what they need from the grocery store. My mom used this to study English.`
             spotlight.className="show";
         }
         else if (e.target == cass4) {
-            spotimg.src="../images/cass/cd.png";
+            spotimg.src="cass/cd.png";
             title.innerHTML=`"Love Song"`;
             info.innerHTML=`A heartfelt Chinese love song. One of my mom's favorites.`
             spotlight.className="show";
