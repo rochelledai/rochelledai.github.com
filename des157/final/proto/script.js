c8364590942d57e2baf856402f341ab55ba5eb9a
Rochelle Dai <rmdai@ucdavis.edu>
2021-03-04 01:15:13 -0800

    updated proto

--- des157/final/proto/script.js
+++ des157/final/proto/script.js
@@ -2,5 +2,15 @@
     "use strict";
     console.log("reading js");
     
+    const things = document.getElementById("container");
+    let nurse = document.getElementById("nurse");
+
+    things.addEventListener('click', function(e){
+        if (e.target == nurse) {
+            nurse.className = "show";
+        }
+    });
+
+    
    
 })();
