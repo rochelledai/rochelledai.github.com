4ac72a12f5738058e552a3a651b8bd0cf0cc6b8a
rochelledai <rmdai@ucdavis.edu>
2022-01-18 01:31:18 -0800

    adding studio1 project

---
+++ des157b/studio1/script.js
@@ -0,0 +1,27 @@
+(function () {
+    'use strict';
+
+    const myVideo = document.querySelector('#myVideo');
+    const water = document.querySelector('#water');
+    const myAudio = new Audio('media/stream.mp3');
+    let ypos;
+    const loading = document.querySelector('#loader');
+    const instruct = document.querySelector('#instruct');
+
+    window.addEventListener('mousemove', function(event){
+        ypos = event.clientY;
+        water.style.opacity = ypos / 300;
+        myAudio.volume = ypos / 1000; // audio needs to be value between 0 and 1. 
+    });
+
+    myVideo.addEventListener('playing', function(){
+        loading.className='hide';
+        instruct.className='show';
+    });
+
+    instruct.addEventListener('click', function(){
+        myAudio.play();
+        instruct.className='hide';
+    })
+
+})();
